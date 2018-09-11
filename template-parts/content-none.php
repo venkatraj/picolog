986dc121b6275d644093dfc58078e7b4715ccee0
PremalathaKumar <premalatha@webulous.in>
2018-09-11 15:27:00 +0530

    Gutenberg Style added

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -16,9 +16,9 @@
 
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'picolog' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
+			
+			<p><?php printf('%1$s <a href="%2$s">%3$s</a>',__('Ready to publish your first post?','picolog'),esc_url( admin_url( 'post-new.php' ) ), __('Get Started Here','picolog'));?></p>
+		
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'picolog' ); ?></p>
@@ -26,7 +26,7 @@
 
 		<?php else : ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'picolog' ); ?></p>
+			<p><?php _e( 'It seems we can\'t find what you\'re looking for. Perhaps searching can help.', 'picolog' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
